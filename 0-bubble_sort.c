3ec3b2e5af118d11707d62d0da32ea8802e1e41b
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2020-02-02 19:41:39 +0000

    [NEW] Add swap function to file, account for gcc

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -20,4 +20,20 @@ void bubble_sort(int *array, size_t size)
 			}
 		}
 	}
-}
+/**
+ * swapper - swaps two nodes
+ * @x: left node
+ * @y: right node
+ * Return: N/A
+ */
+void swapper(int *x, int *y)
+{
+	int temp;
+
+	if (x == y)
+		return;
+
+	temp = *x;
+	*x = *y;
+	*y = temp;
+}}
