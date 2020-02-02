f27e4af5829b6db634892abdd402a2584ae94664
Flavio Vilchez-Espinoza <fespholberton@gmail.com>
2020-02-02 19:50:01 +0000

    [UPDATE] Fix for betty

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -3,28 +3,27 @@
  * bubble_sort - sorting algorithm
  * @array: to be sorted
  * @size: size of array
- * Return: void
  */
 void bubble_sort(int *array, size_t size)
 {
 	size_t fir, sec = 0;
 
-	for(fir = 1; fir < size; fir++)
+	for (fir = 1; fir < size; fir++)
 	{
-		for(sec = 0; sec < size - 1; sec++)
+		for (sec = 0; sec < size - 1; sec++)
 		{
-			if(array[sec] > array[sec + 1])
+			if (array[sec] > array[sec + 1])
 			{
 				swapper(&array[sec], &array[sec + 1]);
 				print_array(array, size);
 			}
 		}
 	}
+}
 /**
  * swapper - swaps two nodes
  * @x: left node
  * @y: right node
- * Return: N/A
  */
 void swapper(int *x, int *y)
 {
@@ -36,4 +35,4 @@ void swapper(int *x, int *y)
 	temp = *x;
 	*x = *y;
 	*y = temp;
-}}
+}
